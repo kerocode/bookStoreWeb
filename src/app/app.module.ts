d96c09dd08fdc6041dd2410fccde1efd88a76969
kerocode <keroleshakem@gmail.com>
2016-09-18 00:32:57 -0400

    add material packages
    add angular 2 material packages
    add hammerjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,47 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { MdButtonModule } from '@angular2-material/button';
+import {MdCardModule}from '@angular2-material/card';
+import { MdGridListModule } from '@angular2-material/grid-list';
+import { MdSliderModule } from '@angular2-material/slider';
+import { MdTooltipModule } from '@angular2-material/tooltip';
+import { MdMenuModule } from '@angular2-material/menu';
+import { MdInputModule } from '@angular2-material/input';
+import { MdListModule } from '@angular2-material/list';
+import { MdCheckboxModule } from '@angular2-material/checkbox';
+import { MdToolbarModule } from '@angular2-material/toolbar';
+import { MdTabsModule } from '@angular2-material/tabs';
+import { MdProgressCircleModule } from '@angular2-material/progress-circle';
+import { MdSlideToggleModule } from '@angular2-material/slide-toggle';
+import { MdRadioModule } from '@angular2-material/radio';
+ 
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdButtonModule,
+    MdCardModule,
+    MdCheckboxModule,
+    MdGridListModule,
+    MdInputModule,
+    MdCardModule,
+    MdListModule,
+    MdMenuModule,
+    MdProgressCircleModule,
+    MdSliderModule,
+    MdSlideToggleModule,
+    MdRadioModule,
+    MdTabsModule,
+    MdTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home-component/home-component.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { HomeComponentComponent } from './home-component.component';
+
+describe('Component: HomeComponent', () => {
+  it('should create an instance', () => {
+    let component = new HomeComponentComponent();
+    expect(component).toBeTruthy();
+  });
+});
